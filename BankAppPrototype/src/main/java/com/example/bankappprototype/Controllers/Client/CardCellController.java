698b2072ab362c283509bb50c4d0840931eb98c3
PascalTaurer <pascal.taurer@hotmail.com>
2024-06-03 23:23:01 +0200

    #25

---
+++ BankAppPrototype/src/main/java/com/example/bankappprototype/Controllers/Client/CardCellController.java
@@ -0,0 +1,20 @@
+package com.example.bankappprototype.Controllers.Client;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CardCellController implements Initializable {
+
+    public Label pin_lbl;
+    public Label iban_lbl;
+    public Label seq_num_lbl;
+    public Label sec_num_lbl;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ BankAppPrototype/src/main/java/com/example/bankappprototype/Controllers/Client/CardsController.java
@@ -0,0 +1,17 @@
+package com.example.bankappprototype.Controllers.Client;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CardsController implements Initializable {
+
+    public ListView cards_listview;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
